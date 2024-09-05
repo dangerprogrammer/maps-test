2fb1d69402bee574d7ce4a404ba1e51963c80132
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2024-09-05 14:09:28 -0300

    Fix some bugs

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,26 @@
 'use client';
 
-import React, { useEffect } from "react";
-import { Loader } from "@googlemaps/js-api-loader";
+import { LatLngExpression } from "leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 function Home() {
-  const mapRef = React.useRef(null);
+  const position: LatLngExpression = [51.505, -0.09];
 
-  useEffect(() => {
-    const initMap = async () => {
-      const loader = new Loader({
-        apiKey: '',
-        version: 'weekly'
-      });
-
-      const { Map } = await loader.importLibrary('maps');
-      
-      const position = {
-        lat: 45,
-        lng: -79
-      };
-
-      // const mapOptions: google.maps.MapOptions = {
-      //   center: position,
-      //   zoom: 16,
-      //   mapId: 'NEXTJS_MAPID'
-      // };
-    };
-  });
-
-  return (
+  return <>
     <h1>Hello, World!</h1>
-  );
+    <section className="map-container">
+    <MapContainer center={position} zoom={13} scrollWheelZoom={!1}>
+      <TileLayer
+        attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS popup.
+        </Popup>
+      </Marker>
+    </MapContainer>
+    </section>
+  </>
 }
 
 export default Home;
