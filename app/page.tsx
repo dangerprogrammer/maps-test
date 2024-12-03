4eb0ecc9c2bc808a6f319047120ef8982d8aa460
Patrick <104269678+dangerprogrammer@users.noreply.github.com>
2024-12-03 08:50:39 -0300

    Update some properties

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import { LatLngExpression } from "leaflet";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { useEffect, useState } from "react";
 import { device_result } from "@/types";
+import {MapComponent} from "@/components/map-component";
 
 function Home() {
   const position: LatLngExpression = [51.505, -0.09];
@@ -12,34 +12,23 @@ function Home() {
 
   useEffect(() => {
     async function getGPS() {
-      const data = await fetch('http://127.0.0.1:8000/api/contentors/');
-      const gps = (await data.json()) as device_result[];
+      const data = await fetch('http://127.0.0.1:8000/api/contentors/'), gps = (await data.json()),
+        { results } = gps;
 
       setIsLoading(!1);
-      setDeviceResults(gps);
-      console.log(gps);
+      setDeviceResults(results);
+      // console.table(results[0]);
     };
 
     getGPS();
   }, []);
 
-  if (isLoading) {
-    return <div> Carregando...</div>;
-  }
+  if (isLoading) return <div>Carregando...</div>;
 
   return <>
     <h1>Hello, World!</h1>
     <section className="map-container">
-      <MapContainer center={position} zoom={13} scrollWheelZoom={!1}>
-        <TileLayer
-          attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS popup.
-          </Popup>
-        </Marker>
-      </MapContainer>
+      <MapComponent position={position}></MapComponent>
     </section>
   </>
 }
