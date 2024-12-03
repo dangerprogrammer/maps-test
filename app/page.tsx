19dd7e620a47434440b30d4cce9b7618be8cd799
Patrick <104269678+dangerprogrammer@users.noreply.github.com>
2024-12-03 10:51:32 -0300

    ...

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ function Home() {
   return <>
     <main className="flex items-center justify-center min-h-screen">
       <span className="flex items-center justify-center absolute bg-black h-full w-full">
-        <video className="opacity-50 flex object-cover pointer-events-none h-full w-full" autoPlay playsInline disablePictureInPicture src="./background-video.mp4"></video>
+        <video className="opacity-50 flex object-cover pointer-events-none h-full w-full" autoPlay loop disablePictureInPicture src="./background-video.mp4"></video>
       </span>
       <h1 className="text-6xl font-extrabold max-w-[50vw] text-center z-[100]">Saiba para onde o lixo vai com a <span className="font-black text-green-400 text-nowrap">Smart Container</span></h1>
     </main>
