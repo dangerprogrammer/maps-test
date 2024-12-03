34313103ca0b481de22740bcfcaee919f62f2598
Patrick <104269678+dangerprogrammer@users.noreply.github.com>
2024-12-03 10:48:19 -0300

    Starting initial page

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@
 import { LatLngExpression } from "leaflet";
 import { useEffect, useState } from "react";
 import { device_result } from "@/types";
-import MapComponent from "@/components/map-component";
 import dynamic from "next/dynamic";
 
 function Home() {
@@ -19,21 +18,25 @@ function Home() {
 
       const gps = await data.json(), { results } = gps;
 
-      setIsLoading(!1);
+      setTimeout(() => setIsLoading(!1), 1e3);
       setDeviceResults(results);
-      console.table(results[0]);
+      console.clear();
+      console.table(results);
     })();
   }, []);
-
-  if (isLoading) return <div>Carregando...</div>;
 
   const MapContent = dynamic(() => import('@/components/map-component'), { ssr: !1 });
 
   return <>
-    <h1>Hello, World!</h1>
-    <section className="map-container">
-      <MapContent position={position}></MapContent>
-    </section>
+    <main className="flex items-center justify-center min-h-screen">
+      <span className="flex items-center justify-center absolute bg-black h-full w-full">
+        <video className="opacity-50 flex object-cover pointer-events-none h-full w-full" autoPlay playsInline disablePictureInPicture src="./background-video.mp4"></video>
+      </span>
+      <h1 className="text-6xl font-extrabold max-w-[50vw] text-center z-[100]">Saiba para onde o lixo vai com a <span className="font-black text-green-400 text-nowrap">Smart Container</span></h1>
+    </main>
+    <main className="flex items-center justify-center min-h-screen">
+      {isLoading ? <div>Carregando...</div> : <MapContent position={position}></MapContent>}
+    </main>
   </>
 }
 
--- components/map-component.tsx
+++ components/map-component.tsx
@@ -3,12 +3,14 @@ import { ReactElement } from "react";
 import { MapContainer, TileLayer } from "react-leaflet";
 
 function MapComponent({ position, children }: { position: LatLngExpression, children?: ReactElement }) {
-    return <MapContainer center={position} zoom={13} scrollWheelZoom={!1}>
+    return <section className="map-container">
+        <MapContainer center={position} zoom={13} scrollWheelZoom={!1}>
         <TileLayer
             attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
         {children}
     </MapContainer>
+        </section>
 };
 
 export default MapComponent;
